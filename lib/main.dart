b40d5cd9c78af7e75c2f02f3e89d757dc461e8cc
john <jonwanjema@gmail.com>
2020-01-23 09:24:55 +0300

    added a statfull widget

--- lib/main.dart
+++ lib/main.dart
@@ -89,3 +89,15 @@ class Home extends StatelessWidget {
     );
   }
 }
+class Test extends StatefulWidget {
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
